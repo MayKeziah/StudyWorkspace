bebec002b944be1e5cc60b0903e382a10dc1f0e7
MayKeziah <45299665+MayKeziah@users.noreply.github.com>
2019-02-06 02:52:19 -0800

    create cube

---
+++ LinearGrouping/src/rubixCode/Cube.java
@@ -0,0 +1,13 @@
+package rubixCode;
+
+public class Cube {
+	private Face[] cube = new Face[6];
+	
+	/**
+	 * Constructor: default, null cube
+	 * */
+	public Cube() {}
+	
+	
+
+}
--- LinearGrouping/src/rubixCode/rubixCodeClient.java
+++ LinearGrouping/src/rubixCode/rubixCodeClient.java
@@ -3,7 +3,7 @@ package rubixCode;
 public class rubixCodeClient {
 	public static void main(String[] args) {
 		Face test = new Face(Color.GREEN);
-		System.out.println(test);
+		System.out.println("" + test + test.isSolid());
 		
 	}
 
