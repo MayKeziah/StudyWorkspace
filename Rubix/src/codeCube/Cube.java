a783ba51cfb71787f905b9c338922978018163a7
MayKeziah <45299665+MayKeziah@users.noreply.github.com>
2019-02-12 22:10:00 -0800

    finish reset methods, have not tested

--- Rubix/src/codeCube/Cube.java
+++ Rubix/src/codeCube/Cube.java
@@ -9,6 +9,10 @@ package codeCube;
  */
 public class Cube {
 	private Color[] rubix =  new Color[54];
+	private final int[] sideIndexZero = {9, 12, 15, 18};
+	private final Color[] colorChoice = {Color.BLUE, Color.GREEN, Color.ORANGE, Color.RED, 
+										Color.WHITE, 
+										Color.YELLOW};
 	
 	public Cube() {
 		resetCube();
@@ -16,42 +20,34 @@ public class Cube {
 	
 	public void resetCube() {
 		resetTop();
-		reset1();
-		reset2();
-		reset3();
-		reset4();
+		resetSides();
 		resetBase();
+	}
+
+	private void resetSides() {
+		for(int side = 0; side < 4; side++) {
+			resetSide(sideIndexZero[side], colorChoice[side]);
+		}
 	}
 
 	private void resetBase() {
 		for (int i = 45; i < 53; i++) {
-			rubix[1] = Color.YELLOW;
+			rubix[1] = colorChoice[5];
 		}
 	}
 
 	private void resetTop() {
 		for(int i = 0; i < 8; i++) {
-			rubix[i] = Color.WHITE;
+			rubix[i] = colorChoice[4];
 		}
 	}
 
-	private void reset1() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void reset2() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void reset3() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private void reset4() {
-		// TODO Auto-generated method stub
+	private void resetSide(int start, Color color) {
+		for(int i = start; i < start + 3; i++) {
+			for (int j = i; j < start + 16; j += 12) {
+				rubix[j] = color;
+			}
+		}
 		
 	}
 }
